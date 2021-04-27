76a5faf1b1879776161272ced3135cad18b5ef34
ErikDRen <ren.erik.19@gmail.com>
2021-04-27 14:19:13 +0200

    :tada: i think the server is 80% ready :tada:

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 	"text/template"
 )
 
 func main() {
 	http.HandleFunc("/", hom)
 	//http.HandleFunc("/about", abo)
-	http.Handle("/stuff/", http.StripPrefix("/stuff", http.FileServer(http.Dir("assets"))))
+	// http.Handle("/stuff/", http.StripPrefix("/stuff", http.FileServer(http.Dir("assets"))))
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -27,15 +23,7 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 func hom(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "default.html", Ascii)
-	arg := r.FormValue("Arg")
-	template := r.FormValue("Template")
-	asciiLetters, err := mapping(template) // recover the map
-	if err != nil {
-		//Do Something
-	}
-	phrase := []rune(arg) // create a slice of rune who have every character
-	Ascii(phrase, asciiLetters, w)
+	tpl.ExecuteTemplate(w, "index.html", Ascii)
 }
 
 // func abo(w http.ResponseWriter, r *http.Request) {
